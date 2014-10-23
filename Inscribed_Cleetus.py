41c46e3867cb878ac99b5f41de928595b5c02adb
Ananya Cleetus <ananyacleetus@gmail.com>
2014-10-22 20:18:59 -0400

    Inscribed_Cleetus.py

---
+++ Inscribed_Cleetus.py
@@ -0,0 +1,31 @@
+import math 
+
+sides = eval(raw_input("how many sides? ")) #number of sides 
+radius= eval(raw_input("how long is radius? ")) #radius
+
+def calculateAngle(sides):
+	angle = math.pi/sides
+	return angle
+
+def calculateSideLength(angle, radius):
+	slength = 2*radius*(math.cos(angle))
+	return slength
+
+def calculateHeight(angle, radius):
+	height = radius*math.sin(angle)
+	return height 
+
+def calculateArea(height, slength, sides):
+	area = sides*(height*slength/2)
+	return area
+
+def getArea(sides, radius):
+	angle = calculateAngle(sides)
+	slength = calculateSideLength(angle, radius)
+	height = calculateHeight(angle, radius)
+	area = calculateArea(height, slength, sides)
+	return area
+
+print "The area of a(n) " + str(sides) + "-sided polygon with radius " + str(radius) + " is " + str(getArea(sides, radius)) + "."
+
+
