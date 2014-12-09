5be40e9cd8747c35adbc282cd30b40efeebb33df
Ananya Cleetus <ananyacleetus@gmail.com>
2014-12-09 13:25:49 -0500

    audio

--- Music Organizer/MusicOrganizer.java
+++ Music Organizer/MusicOrganizer.java
@@ -5,69 +5,69 @@ import entagged.audioformats.exceptions.*;
 
 /**
  * A class to hold details of audio files.
- * 
+ *
  * @author David J. Barnes and Michael Kölling
  * @version 2011.07.31
  */
-public class MusicOrganizer
-{
+public class MusicOrganizer {
     // An ArrayList for storing the file names of music files.
     private ArrayList<Track> files;
-        
+
     /**
      * Create a MusicOrganizer
      */
-    public MusicOrganizer()
-    {
+    public MusicOrganizer() {
         files = new ArrayList<Track>();
+        File audioDir = new File("Audio");
+        String[] filenames = audioDir.list();
+
+        for (int i = 0; i < filenames.size(); i++) {
+            files.add(filenames[i]);
+        }
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
      */
-    public void addTrack(Track track)
-    {
+    public void addTrack(Track track) {
         files.add(track);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
      */
-    public int getNumberofTracks()
-    {
+    public int getNumberofTracks() {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
      */
-    public void listTrack(int index)
-    {
-         if(validIndex(index) == true) {
+    public void listTrack(int index) {
+        if (validIndex(index) == true) {
             Track track = files.get(index);
             System.out.println("Song Title: " + track.getTitle() + " Artist: " + track.getArtist() + " Genre: " + track.getGenre() + " Year: " + track.getYear() + ".");
         }
     }
-/**
-     * Lists all files 
-     * @param none
-     */
-    public void listAllTracks(){
-        for(int i=0; i<files.size(); i++){
-                listTrack(i);
-        } 
-           }
-    
+    /**
+         * Lists all files
+         * @param none
+         */
+    public void listAllTracks() {
+        for (int i = 0; i < files.size(); i++) {
+            listTrack(i);
+        }
+    }
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
      */
-    public void removeTrack(int index)
-    {
-        if(validIndex(index) == true) {
+    public void removeTrack(int index) {
+        if (validIndex(index) == true) {
             files.remove(index);
         }
     }
@@ -76,32 +76,33 @@ public class MusicOrganizer
      * Checks if index is valid
      * @param index to be checked
      */
-    public boolean validIndex(int index){
-        if (index >= 0 && index < files.size()){
+    public boolean validIndex(int index) {
+        if (index >= 0 && index < files.size()) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
+    // /**
+    //  * Returns list of tracks with that artist
+    //  * @param artist to be searched for
+    //  */
+    // public ArrayList<Track> getTracksByArtist(String artist){
+    //     artistSearch = new ArrayList<Track>;
+    //      for (int i = 0; i < files.size(); i++) {
+    //         if (file)
+    // }
 
 
-public static void main(String [ ] args){
+    public static void main(String [ ] args) {
 
-    MusicOrganizer mus = new MusicOrganizer();
-    mus.addTrack(new Track("track1.mp3"));
-    mus.addTrack(new Track("track2.mp3"));
-    System.out.println(mus.getNumberofTracks());
-    mus.listTrack(0);
-    mus.listTrack(1);
-    mus.listTrack(0);
-    mus.listTrack(0);
-    mus.addTrack(new Track("track3.mp3"));
-    mus.addTrack(new Track("track4.mp3"));
-    mus.addTrack(new Track("track5.mp3"));
-    mus.listAllTracks();
+        MusicOrganizer mus = new MusicOrganizer();
+        System.out.println(mus.getNumberofTracks());
+        mus.listTrack(0);
+        mus.listTrack(1);
+        mus.listAllTracks();
 
 
-}
+    }
 }
--- Music Organizer/Track.java
+++ Music Organizer/Track.java
@@ -1,73 +1,80 @@
 import java.util.ArrayList;
 /**
  * A class to hold details of audio files.
- * 
+ *
  * @author Ananya Cleetus
  * @version 2014.12.07
  */
-public class Track
-{
-	private String filename;
-	private String title;
-	private String artist;
-	private String genre;
-	private String year;
+public class Track {
+    private String filename;
+    private String title;
+    private String artist;
+    private String genre;
+    private String year;
 
 
 
-/**
-     * Create a Track
-     */
-public Track(String file){
+    /**
+         * Create a Track
+         */
+    public Track(String file) {
+        try {
+            AudioFile audioFile = AudioFileIO.read(new File(file)); //Reads the given file.
+        }
 
-		filename = file;
-		title ="";
-		artist ="";
-		genre = "";
-		year = "";
+        catch (CannotReadException e) {
+            e.printStackTrace();
+        }
 
-}
+        Tag mp3Tag = audioFile.getTag();
 
-/**
-     * Return filename
-     * @param none
-     */
-public String getName(){
-	return filename;
-}
+        filename = file;
+        title = mp3Tag.getFirstTitle();
+        artist = mp3Tag.getFirstArtist();
+        genre = mp3Tag.getFirstGenre();
+        year = mp3Tag.getFirstYear();
+    }
+
+    /**
+         * Return filename
+         * @param none
+         */
+    public String getName() {
+        return filename;
+    }
 
 
-/**
-     * Return title
-     * @param none
-     */
-public String getTitle(){
-	return title;
-}
+    /**
+         * Return title
+         * @param none
+         */
+    public String getTitle() {
+        return title;
+    }
 
-/**
-     * Return artist
-     * @param none
-     */
-public String getArtist(){
-	return artist;
-}
+    /**
+         * Return artist
+         * @param none
+         */
+    public String getArtist() {
+        return artist;
+    }
 
-/**
-     * Return genre
-     * @param none
-     */
-public String getGenre(){
-	return genre;
-}
+    /**
+         * Return genre
+         * @param none
+         */
+    public String getGenre() {
+        return genre;
+    }
 
-/**
-     * Return year
-     * @param none
-     */
-public String getYear(){
-	return year;
-}
+    /**
+         * Return year
+         * @param none
+         */
+    public String getYear() {
+        return year;
+    }
 
 
 
